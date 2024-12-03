f49a3d1c0c904902e0346abf62d8ceb7e9ab014b
marcapedoh <adralabel@gmail.com>
2024-12-03 06:41:29 +0000

    custom observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject, DestroyRef, signal, effect } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,20 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   clickCount = signal('0')
   clickCount$ = toObservable(this.clickCount)
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
 
+      if (timesExecuted > 3) {
+        clearInterval(interval)
+        subscriber.complete();
+        return;
+      }
+      console.log("Emiting new value...")
+      subscriber.next({ message: "new Value" })
+      timesExecuted++;
+    }, 2000)
+  })
   interval$ = interval(1000)
   intervalSignal = toSignal(this.interval$, { initialValue: 0, manualCleanup: true })
   constructor() {
@@ -31,6 +44,10 @@ export class AppComponent implements OnInit {
      this.destroyRef.onDestroy(() => {
        subscription.unsubscribe()
      })*/
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log("COMPLETED")
+    })
 
     const subscription = this.clickCount$.subscribe({
       next: (val) => console.log('Clicked button ', val)
